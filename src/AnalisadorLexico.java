7097a7d56706667ae951f4988ab9182c9e077076
almirph <phelipear@gmail.com>
2022-05-15 14:29:17 -0300

    leitor de arquivo helper

--- src/AnalisadorLexico.java
+++ src/AnalisadorLexico.java
@@ -1,15 +1,21 @@
+import Classes.LeitorDeArquivoHelper;
 import Classes.Token;
 import Enums.TipoTokenEnum;
+
+import java.io.*;
 
 /**
  * Main class Analisador Lexico
  **/
 public class AnalisadorLexico {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Token token = new Token(10, 10, TipoTokenEnum.IDENTIFICADOR, "a");
         System.out.println(token.getLinha());
         System.out.println(token.getColuna());
         System.out.println(token.getTipoToken().getDescricao());
         System.out.println(token.getValor());
+
+        System.out.println(LeitorDeArquivoHelper.lerFileData("sintaxe/certo/attrCHAR.lan"));
     }
+
 }
---
+++ src/Classes/LeitorDeArquivoHelper.java
@@ -0,0 +1,25 @@
+package Classes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class LeitorDeArquivoHelper {
+    private static String lerDeInputStream(InputStream inputStream) throws IOException {
+        StringBuilder resultadoStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                resultadoStringBuilder.append(linha).append("\n");
+            }
+        }
+        return resultadoStringBuilder.toString();
+    }
+
+    public static String lerFileData(String filePath) throws IOException {
+        ClassLoader classLoader = LeitorDeArquivoHelper.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("Inputs/".concat(filePath));
+        return lerDeInputStream(inputStream);
+    }
+}
