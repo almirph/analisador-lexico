b0f8c216512821c61d95fe093b47b85c7626a375
Vinicius Sampaio <viniciuscastrosampaio@gmail.com>
2022-05-22 18:27:42 -0300

    ajustando comentario

--- src/Classes/Estados/Estado.java
+++ src/Classes/Estados/Estado.java
@@ -19,7 +19,9 @@ public abstract class Estado {
     }
 
     /**
-     * Essa função deve verificar qual o proximo estado a partir do próximo char, se não tiver próximo estado ela seta finalizou para true e se tiver dado erro seta erro para true, se o estado for o atual retorna this no NextEstado.Estado
+     * Essa função deve verificar qual o proximo estado a partir do próximo char,
+     * se não tiver próximo estado ela seta finalizou para true e se tiver dado
+     * erro seta erro para true, se o estado for o atual retorna this no NextEstado.Estado
      **/
     public NextEstado nextEstado(char charEntrada) {
         return null;
